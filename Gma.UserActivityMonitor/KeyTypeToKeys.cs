4b78f08ef9c5cc5ed1dbef2d1b0916ace0b327a9
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2020-12-04 16:18:41 +0300

    Added KeyType

---
+++ Gma.UserActivityMonitor/KeyTypeToKeys.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Gma.UserActivityMonitor
+{
+    public static class KeyTypeToKeys
+    {
+        public static Keys GetKeys(this KeyType keyType)
+        {
+            return Enum.GetValues(typeof(Keys)).Cast<Keys>().FirstOrDefault(iKeys =>
+                iKeys.ToString().Equals(keyType.ToString(),
+                    StringComparison.InvariantCultureIgnoreCase));
+        }
+    }
+}
